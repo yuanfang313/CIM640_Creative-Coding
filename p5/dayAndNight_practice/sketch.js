ed2a03b98b2eaaf5b88a20c04c12a77a06a78fff
Yuan Fang <yxf313@miami.edu>
2018-09-13 22:29:06 -0400

    rename the files

---
+++ p5/dayAndNight_practice/sketch.js
@@ -0,0 +1,103 @@
+//readme is a intro file
+//distant functions
+
+
+
+var sun;
+var moon;
+var r = 255;
+var g = 255;
+var b = 255;
+
+var cX = 50;
+var cY = 50;
+var cEdge = 20;
+
+var rectX = 50;
+var rectY = 75;
+var rectSize = 100;
+
+function preload(){
+  sun = loadImage('assets/sun.png');
+  moon =loadImage('assets/moon.png');
+}
+function setup() {
+createCanvas(400,400);
+}
+
+function draw() {
+
+  frameRate(1);
+  background(r, g, b);
+  fill(r, g, b);
+  
+  console.log("mouseX:"+ mouseX + "mouseY:" + mouseY);
+
+  //interface
+  textSize(18);
+  text('Move over each area to reveal a surprise!',20,20);
+
+  //image(sun, 100,100);
+  //image(moon, 200,100);
+  line(width/2,0, width/2,height);
+  text('Day', 20,360);
+  text('Night', 350,360);
+
+  //interaction
+ellipse(cX, cY, cEdge, cEdge);
+
+/*
+  if(ture){
+    execute this code
+  }
+*/
+//&& 并集
+
+if(mouseX >0 && mouseX < width/2){
+  console.log('Day Side');
+  image(sun, 100,100);
+
+}
+if(mouseX >= width/2 && mouseX < width){
+  console,log('Night Side');
+  image(moon, 200,100);
+
+
+//area defined by _shape
+
+  var cDist = dist(cX, cY, mouseX, mouseY);
+  console.log("cDist:"+ cDist);
+
+  if (cDist < 10){
+    r = 255;
+    g = 0;
+    b = 0;
+  } else {
+    r = 255;
+    g = 255;
+    b = 255;
+
+//command+/
+
+rect(rectX, rectY, rectSize, rectSize);
+    if (mouseX > rectX && mouseX < rectX + rectSize){
+      if (mouseY > rectY && mouseY < rectY + rectSize){
+        console.log("In here");
+      }
+    }
+if (mouseX > rectX && mouseX < rectX + rectSize && mouseY > rectY && mouseY < rectY+ rectSize){
+  console.log("In here")
+  r = random(256);
+  g = random(0, 256);
+  b = random(256);
+}
+
+
+rect(rectX, rectY, rectSize, rectSize);
+
+  }
+
+
+}
+
+}
